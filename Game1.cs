8e357d3f0714f96db18d4002a6a4984b3281214a
Nathan Barnes <339723215@GoTVDSB.ca>
2024-11-11 09:30:23 -0500

    gfdhdfghdgfh

--- Game1.cs
+++ Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Runtime.InteropServices;
 
 namespace Animation_in_monogame
 {
@@ -9,8 +10,19 @@ namespace Animation_in_monogame
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        Rectangle window;
+
+
+        Texture2D brownTribble;
+        Texture2D greyTribble;
+        Rectangle greyTribbleRect;
+        Rectangle brownTribbleRect;
+        Vector2 greyTribbleSpeed;
+        Vector2 brownTribbleSpeed;
         public Game1()
         {
+            
+
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
@@ -19,6 +31,16 @@ namespace Animation_in_monogame
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            greyTribbleRect = new Rectangle(300, 10, 100, 100);
+            window = new Rectangle(0, 0, 800, 600);
+            greyTribbleSpeed = new Vector2(5, 2);
+
+            brownTribbleRect = new Rectangle(100, 50, 100, 100);
+            brownTribbleSpeed = new Vector2(7, 1);
+
+            _graphics.PreferredBackBufferWidth = window.Width;
+            _graphics.PreferredBackBufferHeight = window.Height;
+            _graphics.ApplyChanges();
 
             base.Initialize();
         }
@@ -26,6 +48,9 @@ namespace Animation_in_monogame
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            greyTribble = Content.Load<Texture2D>("tribblegrey");
+            brownTribble = Content.Load<Texture2D>("tribblebrown");
 
             // TODO: use this.Content to load your game content here
         }
@@ -36,6 +61,14 @@ namespace Animation_in_monogame
                 Exit();
 
             // TODO: Add your update logic here
+            greyTribbleRect.X += (int)greyTribbleSpeed.X;
+            if (greyTribbleRect.Right > window.Width || greyTribbleRect.Left < 0)
+                greyTribbleSpeed.X *= -1;
+            greyTribbleRect.Y += (int)greyTribbleSpeed.Y;
+            if (greyTribbleRect.Top > window.Height || greyTribbleRect.Bottom < 0)
+                greyTribbleSpeed.Y *= -1;
+
+            
 
             base.Update(gameTime);
         }
@@ -45,7 +78,12 @@ namespace Animation_in_monogame
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
 
+            _spriteBatch.Draw(greyTribble, greyTribbleRect, Color.White);
+            _spriteBatch.Draw(brownTribble, brownTribbleRect, Color.White);
+
+            _spriteBatch.End();
             base.Draw(gameTime);
         }
     }
